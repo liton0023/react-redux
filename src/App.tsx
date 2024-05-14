0fa4d1736ef1104656bb817ae50ac5d5731b0069
Md Liton Mahmud <litonmahmud0023@gmail.com>
2024-05-15 02:27:34 +0600

    done redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-// import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
 import { Toaster } from "./components/ui/Toaster";
 import MainLayout from "./layouts/MainLayout";
-// import { auth } from './lib/firebase';
-// import { setLoading, setUser } from './redux/features/user/userSlice';
+import { auth } from "./lib/firebase";
+import { setLoading, setUser } from "./redux/feather/user/userSlice";
+import { useAppDispatch } from "./redux/hooks";
 
 function App() {
-  // const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-  // useEffect(() => {
-  //   dispatch(setLoading(true));
+  useEffect(() => {
+    dispatch(setLoading(true));
 
-  //   onAuthStateChanged(auth, (user) => {
-  //     if (user) {
-  //       dispatch(setUser(user.email!));
-  //       dispatch(setLoading(false));
-  //     } else {
-  //       dispatch(setLoading(false));
-  //     }
-  //   });
-  // }, [dispatch]);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUser(user.email!));
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+      }
+    });
+  }, [dispatch]);
 
   return (
     <div>
